2c66576de3d9a62df946908614421bc5b55925ee
jitasek <jitka.moreira@gmail.com>
2021-12-02 23:51:37 +0100

    register html routes

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ const app = express();
 // set up port
 const PORT = 3000;
 
+// define my folder with static assets
+app.use(express.static(path.join(__dirname, "../public")));
+
 // connect my express app to the port
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
